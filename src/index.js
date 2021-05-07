c058b5b678c5b88d722a5223e7836dae8056db61
Shreya Maheshwari <shreyam803@gmail.com>
2021-05-07 18:09:05 +0530

    initialise express

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const publicDiretorypath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDiretorypath));
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+});
+
