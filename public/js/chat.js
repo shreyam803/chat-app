3db4007f98dfe90fdeca9848a062b6da8142ff56
Shreya Maheshwari <shreyam803@gmail.com>
2021-05-10 18:09:24 +0530

    sending messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,6 +19,7 @@ const { username, room } = Qs.parse(location.search,{ ignoreQueryPrefix: true })
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
     });
@@ -28,6 +29,7 @@ socket.on('message', (message) => {
 socket.on('locationMessage', (message) => {
     console.log(message);
     const html = Mustache.render(locationTemplate, {
+        username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     });
@@ -79,7 +81,7 @@ $sendLocationButton.addEventListener('click', (e) => {
     })
 })
 
-socket.emit('join',{ username, room },()=>{
+socket.emit('join',{ username, room },(error)=>{
     if(error){
         alert(error)
         location.href ='/'
--- src/index.js
+++ src/index.js
@@ -36,27 +36,31 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin','Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined!`))
 
         callback()
 
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
         const filter = new Filter();
 
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed.')
         }
 
-        io.to('india').emit('message', generateMessage(message));
+        io.to(user.room).emit('message', generateMessage(user.username,message));
         callback();
     })
 
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
@@ -66,7 +70,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left!`))
         }
 
     })
